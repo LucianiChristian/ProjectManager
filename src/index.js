faad65523ff0d3294c4f5e200c5d9670a19ede85
LucianiChristian <luciani_christian@yahoo.com>
2022-07-31 19:16:05 -0500

    Handled asset management for css and images.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+import './style.css';
+import kenshin from './kenshin.jpg';
+
 const p = document.querySelector('p');
 
 p.textContent = 'Build successful!';
+
+const div = document.createElement('div');
+document.body.appendChild(div);
+
+const kenshinImage = new Image();
+kenshinImage.src = kenshin;
+div.appendChild(kenshinImage);
