2edba1534499a56734a7e3bd867e895630e8e024
LucianiChristian <luciani_christian@yahoo.com>
2022-08-01 22:12:15 -0500

    Broke the UI into separate objects and separated event listeners into dynamic and static

--- src/components/ui.js
+++ src/components/ui.js
@@ -2,74 +2,80 @@ import {controller, model, view} from './mvc/mvc.js';
 
 export {UI};
 
-const UI = {
-    clearSidebarProjects() {
-      const sidebarProjects = document.getElementById('sidebarProjects');
-      
-      sidebarProjects.innerHTML = '';
-    },
-    addSidebarProjects() {
-      const projectElements = controller.renderProjects();
-      
-      const sidebarProjects = document.getElementById('sidebarProjects');
-      projectElements.forEach(element => {
-        sidebarProjects.appendChild(element);
-      });
-    },
-    refreshSidebarProjects() {
-      this.clearSidebarProjects();
-      this.addSidebarProjects();
-    },
-    addProject(name) {
-      controller.addProject(name);
-      this.refreshSidebarProjects();
-    },
-    removeProject(index) {
-      controller.removeProject(index);
-      this.refreshSidebarProjects();
-    },
-    refreshCurrentProjectTitle() {
-      const titleText = controller.renderCurrentProjectTitle();
-      
-      const currentProjectHeading = document.getElementById('currentProjectTitle');
-      
-      currentProjectHeading.textContent = titleText;
-    },
-    clearCurrentProjectTasks() {
-      const currentProjectTasks = document.getElementById('currentProjectTasks');
-      
-      currentProjectTasks.innerHTML = '';
-    },
-    addCurrentProjectTasks() {
-      const taskElements = controller.renderCurrentProjectTasks();
-      
-      const projectTasks = document.getElementById('currentProjectTasks');
-     
-      taskElements.forEach((element) => {
-        projectTasks.appendChild(element);
-      });
-    },
-    refreshCurrentProjectTasks() {
-      this.clearCurrentProjectTasks();
-      this.addCurrentProjectTasks();
-    },
-    refreshCurrentProject() {
-      this.refreshCurrentProjectTitle();
-      this.refreshCurrentProjectTasks();
-    },
-    refreshDashboard() {
-      this.refreshSidebarProjects();
-      this.refreshCurrentProject();
-      eventListeners.sidebarProjects();
-    },
-    switchCurrentProject(index) {
-      controller.setCurrentProjectIndex(index);
-      this.refreshDashboard();
-    }
+const sideBar = {
+  clearSidebarProjects() {
+    const sidebarProjects = document.getElementById('sidebarProjects');
+    
+    sidebarProjects.innerHTML = '';
+  },
+  addSidebarProjects() {
+    const projectElements = controller.renderProjects();
+    
+    const sidebarProjects = document.getElementById('sidebarProjects');
+    projectElements.forEach(element => {
+      sidebarProjects.appendChild(element);
+    });
+  },
+  refreshSidebarProjects() {
+    this.clearSidebarProjects();
+    this.addSidebarProjects();
+    dynamicEventListeners.sidebarProjects();
+  },
+  addProject(name) {
+    controller.addProject(name);
+    this.refreshSidebarProjects();
+  },
+  removeProject(index) {
+    controller.removeProject(index);
+    this.refreshSidebarProjects();
+  },
+  switchCurrentProject(index) {
+    controller.setCurrentProjectIndex(index);
+    refreshDashboard();
+  }
 }
 
+const currentProject = {
+  refreshCurrentProjectTitle() {
+    const titleText = controller.renderCurrentProjectTitle();
+    
+    const currentProjectHeading = document.getElementById('currentProjectTitle');
+    
+    currentProjectHeading.textContent = titleText;
+  },
+  clearCurrentProjectTasks() {
+    const currentProjectTasks = document.getElementById('currentProjectTasks');
+    
+    currentProjectTasks.innerHTML = '';
+  },
+  addCurrentProjectTasks() {
+    const taskElements = controller.renderCurrentProjectTasks();
+    
+    const projectTasks = document.getElementById('currentProjectTasks');
+   
+    taskElements.forEach((element) => {
+      projectTasks.appendChild(element);
+    });
+  },
+  refreshCurrentProjectTasks() {
+    this.clearCurrentProjectTasks();
+    this.addCurrentProjectTasks();
+  },
+  refreshCurrentProject() {
+    this.refreshCurrentProjectTitle();
+    this.refreshCurrentProjectTasks();
+  },
+}
+
+function refreshDashboard() {
+      sideBar.refreshSidebarProjects();
+      currentProject.refreshCurrentProject();
+}
+
+const UI = {sideBar, currentProject, refreshDashboard};
 
 // ~~~~~~~~~~~~~~~T E S T I N G   B E L O W~~~~~~~~~~~~~~ //
+// will eventually be transferred to index.js once UI has the corresponding addProject and addProjectTask functions
 
 controller.addProject('My Project 1');
 
@@ -88,19 +94,19 @@ controller.addProject('My Project 3');
 controller.addCurrentProjectTask('hi','hi',['clean room', 'do stuff'],'to-do');
 controller.addCurrentProjectTask('hi','hi',['pet da cat'],'to-do');
 
-// ---------------Event Listeners---------- //
-const eventListeners = {
+// ---------------Dynamic Event Listeners---------- //
+const dynamicEventListeners = {
     sidebarProjects : () => {
         const sidebarProjects = document.querySelectorAll('#sidebarProjects > p');
     sidebarProjects.forEach((p) => {
       p.addEventListener('click', () => {
-        UI.switchCurrentProject(p.dataset.index);
+        sideBar.switchCurrentProject(p.dataset.index);
       })
     });
     }
-  }
+}
 
-  // ---------------Event Listeners---------- //
+// ---------------Static Event Listeners---------- //
 // Modal
 const addNewTask = document.getElementById('header-addNewTask');
 const modalContainer = document.getElementById('modalContainer');
